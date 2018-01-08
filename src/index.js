e9e73d986bcae8308bf59b54fef3589205bf1ef2
Lenon Lopez <lenonlopez@outlook.com>
2018-01-07 18:36:51 -0800

    Remove pre written CLI code. Starting fresh.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+const App = function() {
 
-import App from './components/app';
-import reducers from './reducers';
+    return <div> hi </div>;
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+}
