cb941b5e8a6820d4cda1ae613d19b9c66440ebd2
Lenon Lopez <lenonlopez@outlook.com>
2018-01-07 23:00:41 -0800

    Add videolist component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 // components
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBEKl_Hw8tCNlKsdVC8zJsWf9vIRvSKREQ';
 
@@ -18,7 +19,8 @@ class App extends Component {
 	render(){
 		return (
 			<div>
-				<SearchBar/>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
