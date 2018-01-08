acf3d0a21f668f05dd287aad2cd208989e6487cc
Lenon Lopez <lenonlopez@outlook.com>
2018-01-07 22:51:42 -0800

    Refactor app component from functional to class
    - added YTSearch within constructor and initialized state

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 // components
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBEKl_Hw8tCNlKsdVC8zJsWf9vIRvSKREQ';
 
-const App = () => {
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-	return (
-		<div>
-			<SearchBar/>
-		</div>
-	)
+		this.state = { videos: [] };
 
+		YTSearch({ key:API_KEY, term: 'surfboards'}, videos => this.setState({ videos }));
+	}
+	render(){
+		return (
+			<div>
+				<SearchBar/>
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
